19f2ff9311f079d5fb8939cfb8c9041bac7166f8
michi <michele.lodi@blu.it>
2021-12-17 17:21:59 +0100

    A quite huge refactoring

--- app/src/main/java/it/unimi/maledettatreest/LinesFragment.java
+++ app/src/main/java/it/unimi/maledettatreest/LinesFragment.java
@@ -2,65 +2,70 @@ package it.unimi.maledettatreest;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
+import it.unimi.maledettatreest.controller.CommunicationController;
 import it.unimi.maledettatreest.controller.LinesAdapter;
-import it.unimi.maledettatreest.controller.LinesFragmentController;
 import it.unimi.maledettatreest.model.Line;
 import it.unimi.maledettatreest.model.LinesModel;
+import it.unimi.maledettatreest.model.User;
 
 public class LinesFragment extends Fragment {
 
+    private final static String RETRY = "RETRY";
+    private final static String UNEXPECTED_ERROR_MESSAGE = "Something went wrong. Please RETRY";
+    private final static String UNEXPECTED_ERROR_TITLE = "OPS";
     private final String TAG = MainActivity.TAG_BASE + "LinesFragment";
-
-    private LinesFragmentController lfc;
     private Context context;
     private LinesAdapter adapter;
+    private CommunicationController cc;
+    private SharedPreferences prefs;
 
-    public LinesFragment() {
-    }
+    public LinesFragment() {}
 
-    public static LinesFragment newInstance() {
-        return new LinesFragment();
-    }
+    public static LinesFragment newInstance() { return new LinesFragment(); }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG,"onCreate");
         super.onCreate(savedInstanceState);
         context = requireContext();
         adapter = new LinesAdapter(requireActivity(), context);
-        lfc = new LinesFragmentController(this, requireActivity());
+        cc = CommunicationController.getInstance(context);
+        prefs = context.getSharedPreferences(MainActivity.APP_PREFS,0);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.d(TAG,"onCreateView");
-        return inflater.inflate(R.layout.fragment_lines, container, false);
-    }
+                             Bundle savedInstanceState) { return inflater.inflate(R.layout.fragment_lines, container, false); }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        Log.d(TAG,"onViewCreated");
+        Log.d(TAG,"Check first run");
         super.onViewCreated(view, savedInstanceState);
-
-        lfc.applicationSetUp(this::handleGetLinesResponse);
-
+        if(!prefs.contains(User.SID)){
+            Log.d(TAG,"First Run: Setting up application");
+            cc.register(this::registrationResponse, error -> cc.handleVolleyError(error,context,TAG));
+        }else {
+            Log.d(TAG, "Normal Run");
+            if (prefs.contains(Line.DID))
+                Navigation.findNavController(requireActivity(), R.id.nav_host_fragment).navigate(R.id.action_linesFragment_to_boardFragment);
+            else if (LinesModel.getInstance().getSize() == 0)
+                cc.getLines(prefs.getString(User.SID,MainActivity.DOESNT_EXIST), this::handleGetLinesResponse,
+                        error -> cc.handleVolleyError(error,context,TAG));
+        }
         RecyclerView listView = view.findViewById(R.id.postsRecyclerView);
         listView.setLayoutManager(new LinearLayoutManager(context));
         listView.setAdapter(adapter);
@@ -69,11 +74,28 @@ public class LinesFragment extends Fragment {
     @SuppressLint("NotifyDataSetChanged")
     private void handleGetLinesResponse(JSONObject response) {
         Log.d(TAG,"Handling getLines Response");
-
         try {
             for(int i = 0; i < response.getJSONArray("lines").length(); i++)
                 LinesModel.getInstance().addLine(new Line(((JSONObject) response.getJSONArray("lines").get(i))));
             adapter.notifyDataSetChanged();
         } catch (JSONException e) { e.printStackTrace(); }
+    }
+
+    private void registrationResponse(JSONObject response) {
+        Log.d(TAG,"Handling Register Response");
+        try {
+            prefs.edit().putString(User.SID, response.get(User.SID).toString()).apply();
+            Log.d(TAG, "Successfully set up");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d(TAG,"Missing Sid. Server Fucked Up.");
+            new AlertDialog.Builder(context).setMessage(UNEXPECTED_ERROR_MESSAGE)
+                    .setTitle(UNEXPECTED_ERROR_TITLE)
+                    .setPositiveButton(RETRY,
+                            (dialog, id) -> cc.register(this::registrationResponse, error -> cc.handleVolleyError(error,context,TAG)))
+                    .create().show();
+        }
+        cc.getLines(prefs.getString(User.SID,MainActivity.DOESNT_EXIST), this::handleGetLinesResponse,
+                error -> cc.handleVolleyError(error,context,TAG));
     }
 }
--- app/src/main/java/it/unimi/maledettatreest/UserFragment.java
+++ app/src/main/java/it/unimi/maledettatreest/UserFragment.java
@@ -1,5 +1,6 @@
 package it.unimi.maledettatreest;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
@@ -17,26 +18,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.IOException;
+import java.util.Iterator;
 import it.unimi.maledettatreest.controller.CommunicationController;
-import it.unimi.maledettatreest.controller.UserFragmentController;
 import it.unimi.maledettatreest.model.User;
 import it.unimi.maledettatreest.services.ImageManager;
 
 public class UserFragment extends Fragment implements ActivityResultCallback<Uri> {
 
-    private final String TAG = MainActivity.TAG_BASE + "UserFragment";
-
-    private UserFragmentController ufc;
+    private static final String TAG = MainActivity.TAG_BASE + "UserFragment";
     private CommunicationController cc;
     private Context context;
     private SharedPreferences prefs;
-    View view;
-    ActivityResultLauncher<String> getProfilePicture;
+    private View view;
+    private ActivityResultLauncher<String> getProfilePicture;
 
-    public UserFragment() {
-    }
+    public UserFragment() { }
 
     public static UserFragment newInstance() {
         return new UserFragment();
@@ -45,8 +49,6 @@ public class UserFragment extends Fragment implements ActivityResultCallback<Uri
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"onCreate");
-
         context = requireContext();
         prefs = context.getSharedPreferences(MainActivity.APP_PREFS,0);
         getProfilePicture = registerForActivityResult(new ActivityResultContracts.GetContent(), this);
@@ -54,45 +56,96 @@ public class UserFragment extends Fragment implements ActivityResultCallback<Uri
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) { return inflater.inflate(R.layout.fragment_user, container, false); }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        ufc = new UserFragmentController(context, requireActivity(),view, getProfilePicture);
         this.view = view;
-
-        if(prefs.getString(User.NAME, MainActivity.DOESNT_EXIST).equals(MainActivity.DOESNT_EXIST)) {
+        if(!prefs.contains(User.NAME))
             cc.getProfile(prefs.getString(User.SID, MainActivity.DOESNT_EXIST),
-                    response -> ufc.getProfileResponse(response),
-                    error -> cc.handleVolleyError(error, context, TAG));
-        }
-        else {
-            ufc.setupView();
-        }
+                    this::handleGetProfileResponse, error -> cc.handleVolleyError(error, context, TAG));
+        else setupView();
     }
+
+    private void handleGetProfileResponse(JSONObject response) {
+        Log.d(TAG,"Handling GetProfile Response");
+        try {
+            Iterator<String> keys = response.keys();
+            while(keys.hasNext()){
+                String key = keys.next();
+                prefs.edit().putString(key, response.get(key).toString()).apply();
+            }} catch (JSONException e) { e.printStackTrace(); }
+        setupView();
+    }
+
+    private void handleSetProfileResponse(JSONObject jsonObject) { Toast.makeText(context, "Profile successfully updated", Toast.LENGTH_LONG).show(); }
 
     @Override
     public void onActivityResult(Uri uri) {
         Log.d(TAG,"Extracting Bitmap from URI");
-
         try {
             Bitmap bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
             String picture = ImageManager.bitmapToBase64(bitmap);
-            if(picture.length()>137000){
-                new AlertDialog.Builder(context).setMessage("Immagine troppo grande")
-                        .setTitle(MainActivity.ERROR).setPositiveButton(MainActivity.OK, (dialog, id) -> {})
-                        .create().show();
-            }else{
+            if(picture.length()>137000)
+                new AlertDialog.Builder(context).setMessage("Selected image is too large")
+                        .setTitle(MainActivity.ERROR).setPositiveButton(MainActivity.OK, (dialog, id) -> {}).create().show();
+            else{
                 Log.d(TAG,"Setting new image and updating shared preferences");
                 ((ImageView)view.findViewById(R.id.profilePictureIV)).setImageBitmap(bitmap);
                 prefs.edit().putString(User.PICTURE,picture).putString(User.PVERSION,
                         String.valueOf(Integer.parseInt(prefs.getString
                                 (User.PVERSION,MainActivity.DOESNT_EXIST))+1)).apply();
                 cc.setProfile(prefs.getString(User.SID, MainActivity.DOESNT_EXIST), null, picture,
-                        response->ufc.handleSetProfileResponse(response), e->cc.handleVolleyError(e,context,TAG));
+                        this::handleSetProfileResponse, e->cc.handleVolleyError(e,context,TAG));
             }} catch (IOException e) { e.printStackTrace(); }
+    }
+
+    private void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if(inputMethodManager.isAcceptingText()){
+            Log.d(TAG,"Hiding keyboard");
+            inputMethodManager.hideSoftInputFromWindow(requireActivity().getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    private void setupView(){
+        ImageView profilePicture = view.findViewById(R.id.profilePictureIV);
+        EditText profileName = view.findViewById(R.id.profileNameET);
+        Button editName = view.findViewById(R.id.editNameB);
+        Button editPicture = view.findViewById(R.id.editPictureB);
+        String picture = prefs.getString(User.PICTURE, MainActivity.DOESNT_EXIST);
+        profileName.setText(prefs.getString(User.NAME, MainActivity.DOESNT_EXIST));
+        if(picture.equals("null")) {
+            Log.d(TAG, "Using default picture");
+            profilePicture.setImageResource(R.drawable.blank_profile_picture);
+        } else {
+            Log.d(TAG,"Getting profile picture");
+            profilePicture.setImageBitmap(ImageManager.base64ToBitmap(picture));
+        }
+        editName.setOnClickListener(v -> {
+            Log.d(TAG,"Editing name");
+            String name = profileName.getText().toString();
+            if(name.isEmpty()){
+                Log.d(TAG,"User entered en empty name");
+                new AlertDialog.Builder(context).setMessage("Name cannot be empty")
+                        .setTitle(MainActivity.ERROR).setPositiveButton(MainActivity.OK, (dialog, id) -> {})
+                        .create().show();
+            }else if(name.length() > 20){
+                Log.d(TAG,"User entered a name of length " + name.length());
+                new AlertDialog.Builder(context).setMessage("Name cannot exceed 20 characters")
+                        .setTitle(MainActivity.ERROR).setPositiveButton(MainActivity.OK, (dialog, id) -> {})
+                        .create().show();
+            } else {
+                prefs.edit().putString(User.NAME,name).apply();
+                hideSoftKeyboard();
+                cc.setProfile(prefs.getString(User.SID, MainActivity.DOESNT_EXIST), name, null,
+                        this::handleSetProfileResponse, e-> cc.handleVolleyError(e, context, TAG));
+            } });
+        editPicture.setOnClickListener(v -> {
+            Log.d(TAG,"Editing picture");
+            hideSoftKeyboard();
+            getProfilePicture.launch("image/*");
+        });
     }
 }
